8f24aac77f7d0ad81c55f1aca02882035fa2714b
JJJ-maker <1776795871@qq.com>
2020-04-13 21:38:42 +0800

    蒋俊杰--第二周算法题

--- docs/algorithm/code/longestSequence.java
+++ docs/algorithm/code/longestSequence.java
@@ -1,9 +1,9 @@
 public class longestSequence {
     public static void main(String[] args){
         int num=1;
-		//原数组序列
+	//原数组序列
         int[] ints={1,2,4,1,3,4,5,2};
-		//存放每一个元素的满足条件的子序列
+	//存放每一个元素的满足条件的子序列
         int[] counts=new int[ints.length];
         for(int i=0;i<ints.length;i++){
             for(int j=i;j<ints.length-1;j++){
@@ -16,7 +16,7 @@ public class longestSequence {
             counts[i]=num;
             num=1;
         }
-		//获取counts中的最大值
+	//获取counts中的最大值
         int max=1;
         if(counts.length>1){
             max=counts[0];
