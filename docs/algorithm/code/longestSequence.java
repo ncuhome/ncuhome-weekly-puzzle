b0f7c145cba6e8d33ee233689331ec52cfe924f4
JJJ-maker <1776795871@qq.com>
2020-04-14 11:37:43 +0800

    蒋俊杰 第三周算法题答案

--- docs/algorithm/code/longestSequence.java
+++ docs/algorithm/code/longestSequence.java
@@ -1,9 +1,7 @@
 public class longestSequence {
     public static void main(String[] args){
         int num=1;
-	//原数组序列
         int[] ints={1,2,4,1,3,4,5,2};
-	//存放每一个元素的满足条件的子序列
         int[] counts=new int[ints.length];
         for(int i=0;i<ints.length;i++){
             for(int j=i;j<ints.length-1;j++){
@@ -16,7 +14,6 @@ public class longestSequence {
             counts[i]=num;
             num=1;
         }
-	//获取counts中的最大值
         int max=1;
         if(counts.length>1){
             max=counts[0];
@@ -28,9 +25,7 @@ public class longestSequence {
                     }
                 }
             }
-            System.out.println(max);
-        }else{
-            System.out.println(max);
         }
+        System.out.println(max);
     }
 }
